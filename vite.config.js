0187da92243cdde42ca44fbccd7298c95a316a9c
dozyio <github@dozy.io>
2024-10-28 00:55:43 +0000

    release: 0.0.3

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,21 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [dts({
+    insertTypesEntry: true,
+    outputDir: 'dist',
+    tsConfigFilePath: './tsconfig.json'
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CountingSemaphore',
+      fileName: 'counting-semaphore',
       formats: ['es']
     },
     sourcemap: true,
     emptyOutDir: true,
-    minify: true
+    minify: true,
   },
   ignorePatterns: ['test/*'],
   server: {
